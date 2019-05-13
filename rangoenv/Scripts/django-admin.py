ed6c4526b6ad5e96d807d4acb64060a20c0385d4
garciaac01 <garciaac01@gmail.com>
2019-05-13 08:05:11 -0400

    end ch4 - templates and media files

---
+++ rangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\garci\pythonpractice\tango_with_rango\rangoenv\scripts\python3.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
